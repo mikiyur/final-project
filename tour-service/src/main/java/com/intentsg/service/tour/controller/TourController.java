4a4598844f81d9f9aa1d0d1876ff244039554d0f
mikiyur <mikiyur@gmail.com>
2021-04-12 10:04:42 +0300

    Default value for pagination is added

--- tour-service/src/main/java/com/intentsg/service/tour/controller/TourController.java
+++ tour-service/src/main/java/com/intentsg/service/tour/controller/TourController.java
@@ -1,7 +1,6 @@
 package com.intentsg.service.tour.controller;
 
 
-
 import com.intentsg.model.TourDto;
 import com.intentsg.service.tour.entity.Tour;
 import com.intentsg.service.tour.service.TourService;
@@ -29,7 +28,9 @@ public class TourController {
     private TourService tourService;
     private DiscoveryClient discoveryClient;
     private RestTemplate restTemplate;
-    private final int PER_PAGE = 3;
+    private final int TOURS_PER_PAGE = 9;
+    private final String MIN_PRICE = "0";
+    private final String MAX_PRICE = "99999";
 
     @Autowired
     public TourController(TourService tourService, DiscoveryClient discoveryClient, RestTemplate restTemplate) {
@@ -38,14 +39,14 @@ public class TourController {
         this.restTemplate = restTemplate;
     }
 
-    private ServiceInstance getServiceInstance(){
+    private ServiceInstance getServiceInstance() {
         return discoveryClient.getInstances("edge-service").get(0);
     }
 
-	@GetMapping("/test")
-	public ResponseEntity test() {
-		return ResponseEntity.ok("tour-service");
-	}
+    @GetMapping("/test")
+    public ResponseEntity test() {
+        return ResponseEntity.ok("tour-service");
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<TourDto> getTourById(@PathVariable Long id) {
@@ -54,11 +55,11 @@ public class TourController {
     }
 
     @GetMapping("/")
-    public ResponseEntity<Page> getAllTours(@RequestParam(name = "minPrice", required = false) Integer minPrice,
-                                            @RequestParam(name = "maxPrice", required = false) Integer maxPrice,
-                                            @PageableDefault() Pageable pageable) {
+    public ResponseEntity<Page> getAllTours(@RequestParam(name = "minPrice", defaultValue = MIN_PRICE) Integer minPrice,
+                                            @RequestParam(name = "maxPrice", defaultValue = MAX_PRICE) Integer maxPrice,
+                                            @PageableDefault(size = TOURS_PER_PAGE) Pageable pageable) {
         System.out.println(pageable);
-        Page<Tour> page = tourService.getAllTour(minPrice, maxPrice,pageable); //todo max-minPrice default value
+        Page<Tour> page = tourService.getAllTour(minPrice, maxPrice, pageable); //todo max-minPrice default value
         return new ResponseEntity<>(page, HttpStatus.OK);
     }
 }
--- tour-service/src/main/resources/data.sql
+++ tour-service/src/main/resources/data.sql
@@ -1,7 +1,14 @@
 insert into tours(title, description, price)
-values ('tour1', 'some description', 320),
-      ('tour2',  'some description', 210),
-      ('tour3',  'some description', 180),
-      ('tour4',  'some description', 920),
-      ('tour5',  'some description', 1100),
-      ('tour6',  'some Mykytiuk description', 1100);
+values ('tour1', 'some description 13', 320),
+      ('tour2',  'some description 9', 210),
+      ('tour3',  'some description 11', 180),
+      ('tour4',  'some description 3', 920),
+      ('tour5',  'some description 44', 2100),
+      ('tour6',  'some description 7', 400),
+      ('tour7',  'some description 12', 500),
+      ('tour8',  'some description 36', 300),
+      ('tour9',  'some description 42', 1700),
+      ('tour10',  'some description 2', 800),
+      ('tour11',  'some description 5', 450),
+      ('tour12',  'some description 4', 330),
+      ('tour13',  'some  description 3', 240);
